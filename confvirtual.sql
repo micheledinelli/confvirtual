12fa7d016c4a47dadc7cded5d1599a2e771d1d4e
benzebra <benzebra@users.noreply.github.com>
2021-12-16 15:33:31 +0100

    Filippo 16/12 15:33

--- confvirtual.sql
+++ confvirtual.sql
@@ -45,7 +45,66 @@ CREATE TABLE REGISTRAZIONE(
     FOREIGN KEY (Username) REFERENCES UTENTE(Username) ON DELETE CASCADE
 ) ENGINE="INNODB";
 
+CREATE TABLE ADMIN(
+    Username VARCHAR(30)
+    PRIMARY KEY (Username),
+    FOREIGN KEY (Username) REFERENCES UTENTE(Username)
+) ENGINE="INNODB";
 
+CREATE TABLE PRESENTER(
+    Username VARCHAR(30),
+    CurriculumVitae,
+    Foto,
+    NomeUniversità VARCHAR(30),
+    PRIMARY KEY (Username),
+    FOREIGN KEY (Username) REFERENCES UTENTE(Username)
+) ENGINE="INNODB";
 
+CREATE TABLE SPEAKER(
+    Username VARCHAR(30),
+    CurriculumVitae,
+    Foto,
+    NomeUniversità VARCHAR(30),
+    PRIMARY KEY (Username),
+    FOREIGN KEY (Username) REFERENCES UTENTE(Username)
+) ENGINE="INNODB";
 
+CREATE TABLE CREAZIONE(
+    UsernameAdmin VARCHAR(30),
+    AcronimoConferenza VARCHAR(10), 
+    AnnoEdizione int,
+    PRIMARY KEY (UsernameAdmin, AcronimoConferenza, AnnoEdizione),
+    FOREIGN KEY (UsernameAdmin) REFERENCES ADMIN(Username),
+    FOREIGN KEY (AcronimoConferenza, AnnoEdizione) REFERENCES CONFERENZA(Acronimo, AnnoEdizione)
+) ENGINE="INNODB";
 
+CREATE TABLE DATASVOLGIMENTO(
+    AcronimoConferenza VARCHAR(10),
+    AnnoEdizione int,
+    Data DATETIME,
+    PRIMARY KEY (AcronimoConferenza, AnnoEdizione, Data),
+    FOREIGN KEY (AcronimoConferenza, AnnoEdizione) REFERENCES CONFERENZA(Acronimo, AnnoEdizione)
+) ENGINE="INNODB";
+
+CREATE TABLE SESSIONE(
+    Data DATETIME, 
+    Codice VARCHAR(10),
+    Titolo VARCHAR(30),
+    Numero_Presentazioni int,
+    OraInizio DATETIME,
+    OraFine DATETIME, 
+    Link VARCHAR(50)
+    PRIMARY KEY (Codice),
+    FOREIGN KEY (Data) REFERENCES DATASVOLGIMENTO(Data)
+) ENGINE="INNODB";
+
+CREATE TABLE PRESENTAZIONEARTICOLO(
+    CodiceSessione VARCHAR(10),
+    Codice
+    OraInizio DATETIME,
+    OraFine DATETIME,
+    NumeroSequenza int,
+    Tipo VARCHAR(10),
+    PRIMARY KEY (Codice),
+    FOREIGN KEY (CodiceSessione) REFERENCES SESSIONE(Codice)
+)
