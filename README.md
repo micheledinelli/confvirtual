1686d1543899a6e32713f35d8876ecf59ee484ae
micheledinelli <dinellimichele00@gmail.com>
2022-01-26 22:44:14 +0100

    rm all

--- README.md
+++
@@ -1,1 +0,0 @@
-# DBProject2021
--- app.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#close-succesfull-login").addEventListener("click", function(){
-    console.log("done");
-    document.querySelector(".popup-succesfull-login").style.display = "none";
-});
-
