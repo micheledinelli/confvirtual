6b5daf155a5f9db0e766c60605009575165adbed
mussida <awniyoussef3@gmail.com>
2022-01-31 18:38:20 +0100

    PINOP

--- landingPage/functionalities/insertCV.php
+++ landingPage/functionalities/insertCV.php
@@ -9,10 +9,43 @@
 <body>
     <?php
     
-        if(isset($_POST["file"])) {
-            echo "ciao";
-        }
+    session_start();
+
+    if(isset($_FILES["fileCV"])) {
+        $file = $_FILES["fileCV"];
+    }
+
+    try {
+
+        // Connection to db
+        $pdo = new PDO('mysql:host=localhost;dbname=CONFVIRTUAL', $user = 'root', $pass = 'Pinaccio00!');
+        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo -> exec('SET NAMES "utf8"');
         
+        if($_SESSION["userType"] == "SPEAKER") {
+            $query = 'UPDATE CONFVIRTUAL.SPEAKER SET  CurriculumVitae = :lab1  WHERE Username = :lab2';
+        
+        } elseif($_SESSION["userType"] == "PRESENTER") {
+            $query = 'UPDATE CONFVIRTUAL.PRESENTER SET CurriculumVitae = :lab1  WHERE Username = :lab2';
+        } 
+
+        $res = $pdo -> prepare($query);
+        $res -> bindValue(":lab1", $file, PDO::PARAM_LOB);
+        $res -> bindValue(":lab2", $_SESSION["user"]);
+        
+        $res -> execute();
+
+        $_SESSION["opSuccesfull"] = 0;
+    
+    } catch (PDOException $e) {
+        
+        // Errore
+        $_SESSION["error"] = 1;
+
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
 
     
     ?>
