636c67f6e4ac17bc2d4fffe81724dcf578837bae
micheledinelli <dinellimichele00@gmail.com>
2022-03-21 18:19:55 +0100

    terminate le funzionalità eccetto admin

--- functionalities/speaker_presenter.php
+++ functionalities/speaker_presenter.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/form.css">
+
     <title>S-P</title>
 </head>
 <body>
@@ -64,6 +66,39 @@
                 $uniAttuale = $row["NomeUniversità"];
                 $dipAttuale = $row["NomeDipartimento"];
             }
+            
+            $queryTutorialPermessi = 'SELECT * 
+                                    FROM SPEAKER_TUTORIAL AS T, P_TUTORIAL AS PT
+                                    WHERE UsernameSpeaker = :lab1 AND T.CodiceTutorial = PT.CodicePresentazione;';
+            $res = $pdo -> prepare($queryTutorialPermessi);
+            $res -> bindValue(":lab1", $_SESSION["user"]);
+            $res -> execute();
+
+            $tutorialsPermitted = array();
+            while($row = $res -> fetch()) {
+                $tutorial = new StdClass();
+                $tutorial -> titolo = $row["Titolo"];
+                $tutorial -> codice = $row["CodiceTutorial"];
+                array_push($tutorialsPermitted, $tutorial);
+            }
+
+            $queyRisorse = 'SELECT * 
+                        FROM RISORSA, P_TUTORIAL AS PT 
+                        WHERE UsernameSpeaker=:lab1 AND RISORSA.CodiceTutorial = PT.CodicePresentazione;';
+            
+            $res = $pdo -> prepare($queyRisorse);
+            $res -> bindValue(":lab1", $_SESSION["user"]);
+            $res -> execute();
+
+            $risorse = array();
+            while($row = $res -> fetch()) {
+                $risorsa = new StdClass();
+                $risorsa -> link = $row["Link"];
+                $risorsa -> descrizione = $row["Descrizione"];
+                $risorsa -> titoloTutorial = $row["Titolo"];
+                $risorsa -> codiceTutorial = $row["CodiceTutorial"];
+                array_push($risorse, $risorsa);
+            }
 
         }catch(PDOException $e) {
             // Errore
@@ -101,9 +136,9 @@
                     session_start();
                     if($_SESSION['userType'] == "SPEAKER") {
                        print'
-                            <li> <a href="#" onclick="">Inserisci Risorsa</a> </li>
+                            <li> <a href="#" onclick="insertResource()">Inserisci Risorsa</a> </li>
 
-                            <li> <a href="#" onclick="">Modifica Risorsa</a> </li>';
+                            <li> <a href="#" onclick="modifyResource()">Modifica Risorsa</a> </li>';
                     }
                 ?>
                         
@@ -155,12 +190,16 @@
     <!-- JAVASCRIPT -->
     <script>
         
+        const userName = <?php echo json_encode($_SESSION["user"]); ?>
+
         const content = document.getElementById("main-content");
         const userCurriculum = <?php echo json_encode($userCurriculum); ?>;
         const università = <?php echo json_encode($università); ?>;
         const uniAttuale = <?php echo json_encode($uniAttuale); ?>;
         const dipAttuale = <?php echo json_encode($dipAttuale); ?>;
-        
+        const tutorialPermessi = <?php echo json_encode($tutorialsPermitted); ?>;
+        const risorse = <?php echo json_encode($risorse); ?>;
+
         function insertCV() {
             content.innerHTML = `
                 <div class="container text-center w-50">
@@ -312,6 +351,136 @@
             document.getElementById("charNum").innerHTML = obj.value.length + ' / 300';
         }
 
+        function insertResource() {
+            var currentSelection;
+
+            var dynamicContent = '';
+            dynamicContent = `
+                <p class="lead">Seleziona il tutorial per il quale inserirai la risorsa</p>
+                <select id="tutorial-select" class="form-select form-select-lg mb-3" aria-label="select" onchange=produceForm(this.value)>
+                <option value="-1" selected>Tutorial ai quali hai accesso</option>`;
+
+            for(let i = 0; i < tutorialPermessi.length; i++) {
+                titolo = tutorialPermessi[i]["titolo"];
+                codice = tutorialPermessi[i]["codice"];
+                dynamicContent += `<option value=${codice}>${titolo}</option>`;
+            }
+
+            dynamicContent  += `</select>`;
+            content.innerHTML = dynamicContent;
+
+        }
+
+        function produceForm(codiceTutorial) {
+            if(document.getElementById("dynamic-div")) {
+                document.getElementById("dynamic-div").remove();
+            }
+
+            var mySelect = document.getElementById("tutorial-select");
+            var titolo = mySelect.options[mySelect.selectedIndex].text;
+            const div = document.createElement("div");
+            div.classList.add("container");
+            div.classList.add("my-3");
+            div.setAttribute("id", "dynamic-div")
+
+            // Si evita il caso della selzione default
+            if(codiceTutorial !== "-1") {
+                div.innerHTML = `
+                <div class="container-fluid text-center w-50">
+                    <p>Risorsa per ${titolo}</p>
+                    <form action="manageResource.php" method="post" class="container my-5" id="resource-form">
+                        <div class="mb-3 form-group floating">
+                            <input type="text" class="form-control floating" name="link" required autocomplete="off" required>
+                            <label for="nomeConferenza">Link della risorsa</label>          
+                        </div>
+                        <div class="form-floating">
+                            <textarea name="descrizione" form="resource-form" class="form-control" placeholder="Inserisci una breve descrizione" style="height: 200px; required"></textarea>
+                        </div>
+                        <input type="hidden" name="username" value="${userName}">
+                        <input type="hidden" name="codiceTutorial" value="${codiceTutorial}">
+                        <input type="hidden" name="resourceAddOpt" value="add">
+                        <div class="container text-center my-5">
+                            <button type="submit" class="btn btn-primary">Inserisci</button>
+                        </div>
+                    </form>
+                </div>
+                `;
+                content.append(div);
+            }
+        }
+
+        function modifyResource() {
+            content.innerHTML = '';
+            const div = document.createElement('div');
+            div.classList.add('row');
+            var cardContent = "";
+            
+            for(let i = 0; i < risorse.length; i++) {
+                title = risorse[i]["titoloTutorial"];
+                link = risorse[i]["link"];
+                descrizione = risorse[i]["descrizione"];
+                codiceTutorial = risorse[i]["codiceTutorial"];
+                var artificialId = "";
+                artificialId = title.split(" ").join("") + codiceTutorial;
+
+                cardContent += `
+                <div class="card" style="width: 18rem; margin: 5px 5px">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${link}</p>
+                        <p class="small">${descrizione}</p>
+                        <button class="btn btn-primary" data-toggle="modal" data-target="#${artificialId}">Modifica</button>
+                    </div>
+                </div>
+                <div id="${artificialId}" class="modal fade">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modifica ${title}</h5>
+                                    <button type="button" class="btn" data-dismiss="modal" aria-label="Close">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <form action="manageResource.php" method="post" class="container">
+                                        <div class="mb-3 form-group floating">
+                                            <input type="hidden" class="form-control floating" name="username" value=${userName} readonly>
+                                        </div>
+                                        <div class="mb-3 form-group floating">
+                                            <input type="hidden" class="form-control floating" name="codiceTutorial" value=${codiceTutorial} readonly>
+                                        </div>
+                                        <div class="mb-3 form-group floating">
+                                            <input type="text" class="form-control floating" name="link" placeholder="${link}" required>
+                                        </div>
+                                        <div class="mb-3 form-group floating">
+                                            <textarea name="descrizione" class="form-control" placeholder="Edita la descrizione: ${descrizione}" style="height: 200px;" required></textarea>
+                                        </div>
+                                        <input type="hidden" name="resourceAddOpt" value="modify">
+                                        <div class="container text-center">
+                                            <button type="submit" class="btn btn-primary">Modifica</button>
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        </div>
+                                    </form>
+                                    <form method="post" action="deleteResource.php">
+                                        <div class="container my-3 text-center">
+                                            <input type="hidden" name="codiceTutorial" value=${codiceTutorial}>
+                                            <input type="hidden" name="link" value=${link}>
+                                            <input type="hidden" name="username" value=${userName}>
+                                            <button type="submit" class="btn btn-danger">Elimina</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+            }
+            div.innerHTML = cardContent;
+            content.append(div);
+        }
+
+        
         // switch per il menu
         var radio = 0;
         document.getElementById("sidebarCollapse").addEventListener("click", () => {
